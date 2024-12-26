542580d4a15cacb443b788281079679c8aa73b59
diegoforrest <cruzdieeego@gmail.com>
2024-12-26 22:16:01 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Guna2ToggleSwitch1 = New Guna.UI2.WinForms.Guna2ToggleSwitch()
+        Me.LabelEncoderValue = New System.Windows.Forms.Label()
+        Me.Button2 = New Guna.UI2.WinForms.Guna2Button()
+        Me.LabelStartButton = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Guna2ToggleSwitch1
+        '
+        Me.Guna2ToggleSwitch1.BackColor = System.Drawing.Color.Transparent
+        Me.Guna2ToggleSwitch1.CheckedState.BorderColor = System.Drawing.Color.Transparent
+        Me.Guna2ToggleSwitch1.CheckedState.BorderRadius = 10
+        Me.Guna2ToggleSwitch1.CheckedState.FillColor = System.Drawing.Color.Green
+        Me.Guna2ToggleSwitch1.CheckedState.InnerBorderColor = System.Drawing.Color.White
+        Me.Guna2ToggleSwitch1.CheckedState.InnerColor = System.Drawing.Color.White
+        Me.Guna2ToggleSwitch1.Location = New System.Drawing.Point(328, 480)
+        Me.Guna2ToggleSwitch1.Name = "Guna2ToggleSwitch1"
+        Me.Guna2ToggleSwitch1.Size = New System.Drawing.Size(72, 26)
+        Me.Guna2ToggleSwitch1.TabIndex = 0
+        Me.Guna2ToggleSwitch1.UncheckedState.BorderColor = System.Drawing.Color.Transparent
+        Me.Guna2ToggleSwitch1.UncheckedState.BorderRadius = 10
+        Me.Guna2ToggleSwitch1.UncheckedState.FillColor = System.Drawing.Color.Red
+        Me.Guna2ToggleSwitch1.UncheckedState.InnerBorderColor = System.Drawing.Color.White
+        Me.Guna2ToggleSwitch1.UncheckedState.InnerColor = System.Drawing.Color.White
+        '
+        'LabelEncoderValue
+        '
+        Me.LabelEncoderValue.AutoSize = True
+        Me.LabelEncoderValue.BackColor = System.Drawing.Color.Transparent
+        Me.LabelEncoderValue.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.2!)
+        Me.LabelEncoderValue.Location = New System.Drawing.Point(43, 234)
+        Me.LabelEncoderValue.Name = "LabelEncoderValue"
+        Me.LabelEncoderValue.Size = New System.Drawing.Size(111, 35)
+        Me.LabelEncoderValue.TabIndex = 1
+        Me.LabelEncoderValue.Text = "Value: "
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.BorderColor = System.Drawing.Color.Transparent
+        Me.Button2.BorderRadius = 25
+        Me.Button2.DisabledState.BorderColor = System.Drawing.Color.DarkGray
+        Me.Button2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray
+        Me.Button2.DisabledState.FillColor = System.Drawing.Color.FromArgb(CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer), CType(CType(169, Byte), Integer))
+        Me.Button2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer), CType(CType(141, Byte), Integer))
+        Me.Button2.FillColor = System.Drawing.Color.Transparent
+        Me.Button2.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!)
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(96, 308)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(213, 38)
+        Me.Button2.TabIndex = 3
+        '
+        'LabelStartButton
+        '
+        Me.LabelStartButton.AutoSize = True
+        Me.LabelStartButton.BackColor = System.Drawing.Color.Transparent
+        Me.LabelStartButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.2!)
+        Me.LabelStartButton.Location = New System.Drawing.Point(196, 234)
+        Me.LabelStartButton.Name = "LabelStartButton"
+        Me.LabelStartButton.Size = New System.Drawing.Size(113, 35)
+        Me.LabelStartButton.TabIndex = 4
+        Me.LabelStartButton.Text = "Status:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(408, 512)
+        Me.Controls.Add(Me.LabelStartButton)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.LabelEncoderValue)
+        Me.Controls.Add(Me.Guna2ToggleSwitch1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "EXPERIMENT #4"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Guna2ToggleSwitch1 As Guna.UI2.WinForms.Guna2ToggleSwitch
+    Friend WithEvents LabelEncoderValue As Label
+    Friend WithEvents Button2 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents LabelStartButton As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports System.IO.Ports
+Imports MySql.Data.MySqlClient ' Ensure you have the MySQL library imported
+
+Public Class Form1
+
+    Private WithEvents serialPort As New SerialPort("COM6", 9600)
+    Private conn As New MySqlConnection
+    Private CMD As MySqlCommand
+    Private constring As String = "data source = localhost; userid = root; password = ; database = experiment4"
+    Private startReceived As Boolean = False
+    Private isStopped As Boolean = False ' Flag to track if the encoder is stopped
+    Private resetActive As Boolean = False ' Flag to track if reset is active
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        serialPort.BaudRate = 9600
+        serialPort.DataBits = 8
+        serialPort.Parity = Parity.None
+        serialPort.StopBits = StopBits.One
+        serialPort.Handshake = Handshake.None
+
+        Try
+            serialPort.Open()
+        Catch ex As Exception
+            MessageBox.Show("Error opening serial port: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Guna2ToggleSwitch1_CheckedChanged(sender As Object, e As EventArgs) Handles Guna2ToggleSwitch1.CheckedChanged
+        Try
+            If Guna2ToggleSwitch1.Checked Then
+                conn = New MySqlConnection(constring)
+                conn.Open()
+                MsgBox("Successfully Connected to Database experiment4", vbInformation, "Confirmation Window")
+
+            Else
+                If conn IsNot Nothing AndAlso conn.State = ConnectionState.Open Then
+                    conn.Close()
+                    MsgBox("Disconnected from Database experiment4", vbInformation, "Confirmation Window")
+
+                End If
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub serialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles serialPort.DataReceived
+        Dim incomingData As String = serialPort.ReadLine()
+        Debug.WriteLine("Received: " & incomingData)
+
+        Me.Invoke(Sub()
+                      Dim dataParts As String() = incomingData.Split(New String() {": "}, StringSplitOptions.None)
+
+                      If dataParts.Length >= 2 Then
+                          Select Case dataParts(0)
+                              Case "Counter"
+                                  ' Keep updating the counter as long as stop hasn't been pressed
+                                  If Not isStopped Then
+                                      Dim counterValue As Integer
+                                      If Integer.TryParse(dataParts(1), counterValue) AndAlso counterValue >= 0 Then
+                                          LabelEncoderValue.Text = counterValue.ToString()
+
+                                          ' If reset is active and the encoder moves, change the state to "start"
+                                          If resetActive AndAlso counterValue > 0 Then
+                                              LabelStartButton.Text = "start"
+                                              resetActive = False ' Reset the flag since state has changed to start
+                                              Debug.WriteLine("Status changed to 'start' after rotation")
+                                          End If
+                                      End If
+                                  End If
+                              Case "Status"
+                                  Select Case dataParts(1).Trim().ToLower()
+                                      Case "start"
+                                          startReceived = True
+                                          isStopped = False ' Allow updates
+                                          resetActive = False ' Reset should not be active during start
+                                          Debug.WriteLine("Start received: True")
+
+                                      Case "reset"
+                                          ' Reset the counter to 0, set status to reset, and allow encoder movement
+                                          LabelEncoderValue.Text = "0"
+                                          LabelStartButton.Text = "reset"
+                                          resetActive = True ' Set reset flag to true
+                                          startReceived = False
+                                          isStopped = False
+                                          Debug.WriteLine("Reset received: Counter reset to 0")
+
+                                      Case "stop"
+                                          isStopped = True ' Stop updates
+                                          Debug.WriteLine("Stop received: Stopped updates")
+
+                                  End Select
+                                  LabelStartButton.Text = dataParts(1).Trim()
+                          End Select
+                      Else
+                          Debug.WriteLine("Invalid data format received.")
+                      End If
+                  End Sub)
+    End Sub
+
+    Private Sub SaveData()
+        If String.IsNullOrEmpty(LabelEncoderValue.Text) Then
+            MessageBox.Show("No valid data to save. Please rotate the encoder to display a value.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Dim counterValue As Integer = Integer.Parse(LabelEncoderValue.Text)
+        Try
+            If conn IsNot Nothing AndAlso conn.State = ConnectionState.Open Then
+                CMD = New MySqlCommand("INSERT INTO rotary (VALUE, STATUS) VALUES (@Value, @Status)", conn)
+                CMD.Parameters.AddWithValue("@Value", counterValue)
+                CMD.Parameters.AddWithValue("@Status", LabelStartButton.Text)
+                CMD.ExecuteNonQuery()
+
+                MessageBox.Show("Data saved successfully!", "Confirmation", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error saving data: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If serialPort.IsOpen Then
+            serialPort.Close()
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        SaveData()
+    End Sub
+
+
+    Private Sub Button2_Click_1(sender As Object, e As EventArgs) Handles Button2.Click
+        SaveData()
+    End Sub
+End Class
